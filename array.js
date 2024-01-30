7cb2b604763899aa8dc9b9dd50f75478135343ee
SHAYAN ABRAR <shayanabrar7@gmail.com>
2024-01-30 10:36:39 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,19 @@
+//first task
+const fruits=['mango', 'lichi','pineapple','papaya','grape'];
+//console.log(fruits);
+//console.log(fruits[3]);
+const jambura=fruits[2];
+//console.log(jambura);
+//console.log(fruits);
+
+
+
+//second task
+const des=['bandarban', 'shylhet', 'sundarban'];
+//console.log(des);
+des.push('puran dhaka');
+//console.log(des);
+des.push('RAJSHAHI','shajek');
+//console.log(des);
+des.pop('');
+console.log(des);
